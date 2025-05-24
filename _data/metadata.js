d8236addfcb991c426f588fe8f46ac5d16f70fac
Hatem Hosny <hatemhosny@gmail.com>
2025-05-24 06:35:46 +0300

    initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Markdown to LiveCodes",
+	url: "https://markdown-to-livecodes-11ty.pages.dev/",
+	language: "en",
+	description: "",
+	author: {
+		name: "Hatem Hosny",
+		email: "youremailaddress@example.com",
+		url: "https://livecodes.io/docs/markdown-to-livecodes/",
+	},
+};
